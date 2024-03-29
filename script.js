41edb2181ca37ed484b93846f99494b9f740b7e9
Farouk-chtioui <chtiouiaa25@gmail.com>
2024-03-29 18:15:07 +0000

    added more tasks

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function submitForm() {
     const checkboxes = document.getElementsByName('checkbox');
     let response = "";
-    let tasks_imposter=["mto9tl 7ad lin tkaml 5 fake tasks","7awl irb7 ma8ir mto9tl 7ad","o9tel shkoun w hawl to93ed ba7dha lkill hata lin wehd yji aml report w hotha fih","awl kill mta3k lazm tkoun abad ama emergency meeting"  ];
-    let tasks_crew=["susi a9rb wa7d lil bureau mta3k","a9n3 labad bch ykharjo another crew","kol mtkaml task lazmk ta3ti compliment lawl abad trah"];
-    let tasks_security=["ekhdm 4 tasks wmba3d mtnjm tkaml kan ki to9tl chkoun",""];
+    let tasks_imposter=["mto9tl 7ad lin tkaml 5 fake tasks","7awl irb7 ma8ir mto9tl 7ad","o9tel shkoun w hawl to93ed ba7dha lkill hata lin wehd yji aml report w hotha fih","awl kill mta3k lazm ykoun chkoun amal emergency meeting","ki yabdew zouz ba7dha b3adhom emchi ba7dheom w 9oul eli fama wa7ed fehom jbed grafeuze w 5abeha fisa3","a5tar ayy wa7ed w 9oul rani ritou 5arej mel vent","9ouol blasa 8alta mta3 koll kill ta3melha","mato9telch el security","awel mayoufa cooldown mta3 el kill loula lezmek ta3mel kill o5ra mahma tabda el situation eli enti feha","Alone tour7 kaml a3ml a9l contact m3a players","andkch 7a9 tnzl Z tour7 kaml emchi kan al jnébet wala btweli"];
+    let tasks_crew=["susi a9rb wa7d lil bureau mta3k","a9n3 labad bch ykharjo another crew","kol mtkaml task lazmk ta3ti compliment lawl abad trah","tour7 kaml lazmk tkthb","9oul aks ili chofto","kan rit imposter 9tal 9odamk mandkch 7a9 t9oul alih ila fl last round","a9n3 nés ili enti imposter kan kharjouk vote trb7 task mta3k","play normal",""];
+    let tasks_security=["ekhdm 4 tasks wmba3d mtnjm tkaml kan ki to9tl chkoun","7awel to9tel chkoun wa7dou fi terkina w mat5ali 7ad yfi9 bik","awel wa7ed trah mouch ya3mel fi task o9tlou","mata3mel 7ata chy fel game wjuste tab9a ba7dha el vents wtestana chkoun yod5el wela yo5rej w to9etlou","ki tl9a body o93d b7thaha wawl wa7d yji bch yreporti 9oul ili houwa imposter wkan votewh trb7 task mta3k"];
 
     checkboxes.forEach((checkbox, index) => {
       if (checkbox.checked) {
